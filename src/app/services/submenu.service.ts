18b9eea6186f5145aaa5222128a96564e9cc3e54
Josselinmailyn092 <mailynjoss@gmail.com>
2024-11-13 14:49:36 -0500

    Guardando cambios antes de actualizar Angular

---
+++ src/app/services/submenu.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import path from 'path';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubmenuService {
+private subMenuItems =[
+  {
+    label:'INICIO',
+    path:'/inicio',
+    subMenu: []
+  },
+  
+  {
+    label :'LICORES',
+    path: '/licores',
+    subMenu:[
+        {label:'VINOS',path :'vinos'},
+        {label:'CERVEZA',path :'cerveza'},
+        {label:'WHISKEY',path :'whiskey'},
+        {label:'BRANDY',path :'/brandy'},
+        {label:'VODKA',path :'/vodka'},
+        {label:'TEQUILA',path :'/tequila'},
+        {label:'GIN',path :'/gin'}
+
+      
+    ]
+  },
+
+  //confiteria submenu 
+  {
+    label:'CONFITERIA',
+    path:'/confiteria',
+    subMenu:[
+      {label:'GALLETAS', path:'/confiteria/galletas'},
+      {label:'CHOCOLATES', path:'/confiteria/chocolates'},
+      {label:'CHUPETES', path:'/confiteria/chupetes'},
+      {label:'GOMITAS', path:'/confiteria/gomitas'},
+      {label:'CARAMELOS', path:'/confiteria/caramelos'},
+      {label:'CHICLE', path:'/confiteria/chicle'},
+      {label:'SNAKS', path:'/confiteria/snaks'}
+    ]
+
+  },
+  {
+    label:'PROMOCIONES',
+    path:'/promociones',
+    subMenu: []
+  },
+  {
+    label:'NOSOTROS',
+    path:'/nosotros',
+    subMenu: []
+  },
+  {
+    label:'CONTACTO',
+    path:'/contacto',
+    subMenu: []
+  },
+];
+
+geSubMenuItems() {
+  return this.subMenuItems;
+}
+}
