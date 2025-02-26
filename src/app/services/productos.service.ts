611067b758611db90e145d0153b7308de1454823
Anderson <andersonvaslasquez@gmail.com>
2025-02-25 21:07:12 -0500

    Productos services con get añadidos

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductosService {
+  private url = 'http://localhost:3000/productos';
+  constructor(private httpService: HttpClient) {}
+  // Obtener todos los productos con filtros opcionales
+  obtenerProductos(
+    marca?: string,
+    categegoria?: string,
+    presentacion?: number,
+    esDestacado?: boolean,
+    tieneDescuento?: boolean,
+    tipoProducto?: string
+  ): Observable<any[]> {
+    const params: any = {};
+    if (marca) params.marca = marca;
+    if (categegoria) params.categoria = categegoria;
+    if (presentacion) params.presentacion = presentacion;
+    if (esDestacado) params.esDestacado = esDestacado;
+    if (tieneDescuento) params.tieneDescuento = tieneDescuento;
+    if (tipoProducto) params.tipoProducto = tipoProducto;
+    return this.httpService.get<any[]>(this.url, { params });
+  }
+
+  // Obtener información del producto desde su ID
+  obtenerProducto(id: number): Observable<any> {
+    return this.httpService.get<any>(`${this.url}/${id}`);
+  }
+
+  // Obtener todos los productos según una categoría
+  obtenerProductosCategoria(nombreCategoria: string): Observable<any[]> {
+    return this.httpService.get<any[]>(
+      `${this.url}/categoria/${nombreCategoria}`
+    );
+  }
+
+  //
+}
