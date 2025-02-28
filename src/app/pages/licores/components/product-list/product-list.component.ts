98055f671caa526f2d613bc2c08f259d5ca607b4
Anderson <andersonvaslasquez@gmail.com>
2025-02-28 17:49:07 -0500

    imagenes funcionales

--- src/app/pages/licores/components/product-list/product-list.component.ts
+++ src/app/pages/licores/components/product-list/product-list.component.ts
@@ -1,17 +1,19 @@
-import { Component,Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
   @Input() productos: any[] = [];
   @Output() agregarAlCarrito = new EventEmitter<any>();
-productosPaginados: any;
+  productosPaginados: any;
 
   agregarProductoAlCarrito(producto: any) {
     this.agregarAlCarrito.emit(producto);
   }
-
+  obtenerImagen(nombreImagen: string): any {
+    return 'http://localhost:3000/uploads/' + nombreImagen;
+  }
 }
